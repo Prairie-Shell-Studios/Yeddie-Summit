f63976f74de40a920a12b8e9ff9608262455c598
RockettingTurtle <mcquarriecurtis@gmail.com>
2022-09-17 19:11:55 -0500

    Attempt to parameterize test for timer

--- Yeddie Summit/Assets/Scripts/Timer/SimpleTimer.cs
+++ Yeddie Summit/Assets/Scripts/Timer/SimpleTimer.cs
@@ -22,6 +22,9 @@ namespace PrairieShellStudios.Timer
 
         #region properties
 
+        /// <summary>
+        /// Elapse the time if timer is active and return the value.
+        /// </summary>
         public float CurrentTime 
         { 
             get 
@@ -50,55 +53,62 @@ namespace PrairieShellStudios.Timer
 
         /// <summary>
         /// A timer set to count up from startTime and continue indefinitely.
+        /// If the startTime is specified as negative, they will be inverted.
         /// </summary>
-        /// <param name="startTime">The time that the timer will start at.</param>
+        /// <param name="startTime">A positive time value to start at.</param>
         public SimpleTimer(float startTime)
         {
-            minTime = startTime;
-            currentTime = minTime;
+            minTime = (startTime < 0) ? -startTime : startTime;
+            currentTime = this.minTime;
         }
 
         /// <summary>
         /// A timer set to count up from startTime and expire at stopTime.
+        /// If either start or stop time are negative, they will be inverted.
         /// </summary>
-        /// <param name="startTime">The time to start at.</param>
-        /// <param name="stopTime">The time to expire at.</param>
+        /// <param name="startTime">A positive time value to start at.</param>
+        /// <param name="stopTime">A positive time value to expire at.</param>
         public SimpleTimer(float startTime, float stopTime)
         {
-            minTime = startTime;
-            maxTime = stopTime;
-            timeLimit = maxTime;
-            currentTime = minTime;
+            minTime = (startTime < 0) ? -startTime : startTime;
+            maxTime = (stopTime < 0) ? -stopTime : stopTime;
+            timeLimit = this.maxTime;
+            currentTime = this.minTime;
         }
 
         /// <summary>
-        /// A timer that will either countup to or countdown from the specified maxTime.
+        /// A timer that will either count-up to, or count-down from the specified maxTime.
         /// The default minTime is 0.
+        /// If the maxTime is specified as negative, they will be inverted.
         /// </summary>
-        /// <param name="timerDirection">Whether the timer should count up or down.</param>
-        /// <param name="maxTime">The maximum time for the timer.</param>
+        /// <param name="timerDirection">Whether the timer should count-up or count-down.</param>
+        /// <param name="maxTime">A positive value for the maximum time of the timer.
+        /// This is stop time for a count-up timer or start time for a count-down timer.</param>
         public SimpleTimer(TimerDirection timerDirection, float maxTime)
         {
             direction = timerDirection;
-            this.maxTime = maxTime;
-            timeLimit = (direction == TimerDirection.CountUp) ? maxTime : minTime;
-            currentTime = (direction == TimerDirection.CountUp) ? minTime : maxTime;
+            this.maxTime = (maxTime < 0) ? -maxTime : maxTime;
+            timeLimit = (direction == TimerDirection.CountUp) ? this.maxTime : this.minTime;
+            currentTime = (direction == TimerDirection.CountUp) ? this.minTime : this.maxTime;
         }
 
         /// <summary>
-        /// A timer that will either count up to or down from the maxTime.
-        /// The specified timer will either count from or to the minTime.
+        /// A timer that will either count-up to or count-down from the maxTime.
+        /// The specified timer will either count-up from or count-down to the minTime.
+        /// If either the minTime or maxTime values are negative, they will be inverted.
         /// </summary>
         /// <param name="timerDirection">Whether the timer should count up or down.</param>
-        /// <param name="minTime">The minimum time for the timer.</param>
-        /// <param name="maxTime">The maximum time for the timer.</param>
+        /// <param name="minTime">A positive value for the minimum time for the timer.
+        /// This is start time for a count-up timer or stop time for a count-down timer.</param>
+        /// <param name="maxTime">A positive value for the maximum time for the timer.
+        /// This is stop time for a count-up timer or stop time for a count-down timer.</param>
         public SimpleTimer(TimerDirection timerDirection, float minTime, float maxTime)
         {
             direction = timerDirection;
-            this.minTime = minTime;
-            this.maxTime = maxTime;
-            timeLimit = (direction == TimerDirection.CountUp) ? maxTime : minTime;
-            currentTime = (direction == TimerDirection.CountUp) ? minTime : maxTime;
+            this.minTime = (minTime < 0) ? -minTime : minTime;
+            this.maxTime = (maxTime < 0) ? -maxTime : maxTime;
+            timeLimit = (direction == TimerDirection.CountUp) ? this.maxTime : this.minTime;
+            currentTime = (direction == TimerDirection.CountUp) ? this.minTime : this.maxTime;
         }
 
         #endregion
@@ -106,40 +116,40 @@ namespace PrairieShellStudios.Timer
         #region api
 
         /// <summary>
-        /// Changes the duration of the timer and handles the elapsed time change.
-        /// If a negative value is specified, the duration will not be changed.
-        /// Make sure to Reset timer if you want it to start at the correct time.
+        /// Change the duration of the timer and handles the elapsed time change.
+        /// If a negative value is specified, the value will be inverted.
+        /// NOTE: Make sure to Reset timer if you want it to start at the correct time.
         /// </summary>
-        /// <param name="duration">The new duration that the timer will have</param>
+        /// <param name="duration">A positive value for the new duration that the timer will have</param>
         public void ChangeDuration(float duration)
         {
-            if (duration >= 0)
-            {
-                float durationDifference = Duration() - duration;
-                maxTime += durationDifference;
+            float newDuration = (duration < 0) ? -duration : duration;
+            float durationDifference = Duration() - newDuration;
+            maxTime = newDuration;
 
-                if (isActive && direction == TimerDirection.CountDown)
-                {
-                    currentTime += durationDifference;
-                }
-                else if (direction == TimerDirection.CountUp)
-                {
-                    timeLimit = maxTime;
-                }
+            // increase the value of the current time for an active count-down timer
+            if (isActive && direction == TimerDirection.CountDown)
+            {
+                currentTime += durationDifference;
+            }
+            else if (direction == TimerDirection.CountUp)
+            {
+                timeLimit = maxTime;
             }
         }
 
         /// <summary>
-        /// Calculates the entire duration of the timer without considering elapsed time.
+        /// Calculate the entire duration of the timer without considering elapsed time.
         /// </summary>
-        /// <returns>The total length of the timer.</returns>
+        /// <returns>A positive value representing total length of the timer.
+        /// A -1 value representing an unlimited timer.</returns>
         public float Duration()
         {
             return (timeLimit >= 0) ? maxTime - minTime : -1;
         }
 
         /// <summary>
-        /// Determines whether or not the time on the timer has reached its time limit.
+        /// Determine whether or not the time on the timer has reached its time limit.
         /// </summary>
         /// <returns>True if the time limit has been reached. 
         /// False if the time limit has not been reached.</returns>
@@ -147,7 +157,9 @@ namespace PrairieShellStudios.Timer
         {
             bool hasElapsed = false;
 
-            if (timeLimit >= 0 && CurrentTime >= timeLimit)
+            ElapseTime();
+
+            if (timeLimit >= 0 && currentTime >= timeLimit)
             {
                 hasElapsed = true;
             }
@@ -156,9 +168,9 @@ namespace PrairieShellStudios.Timer
         }
 
         /// <summary>
-        /// Resets the timer.
+        /// Reset the timer.
         /// </summary>
-        /// <returns>The start time for the timer.</returns>
+        /// <returns>A positive value representing the start time for the timer.</returns>
         public float Reset()
         {
             currentTime = (direction == TimerDirection.CountUp) ? minTime : maxTime;
@@ -166,9 +178,9 @@ namespace PrairieShellStudios.Timer
         }
 
         /// <summary>
-        /// Begins the timer.
+        /// Start the timer.
         /// </summary>
-        /// <returns>The start time for the timer.</returns>
+        /// <returns>A positive value representing the start time for the timer.</returns>
         public float Start()
         {
             isActive = true;
@@ -176,9 +188,18 @@ namespace PrairieShellStudios.Timer
         }
 
         /// <summary>
-        /// Stops the timer.
+        /// Get the start time of the timer.
         /// </summary>
-        /// <returns>The current time for the timer.</returns>
+        /// <returns>A positive value the represents what time the timer starts at.</returns>
+        public float StartTime()
+        {
+            return (direction == TimerDirection.CountUp) ? minTime : maxTime;
+        }
+
+        /// <summary>
+        /// Stop the timer.
+        /// </summary>
+        /// <returns>A positive value representing the current time for the timer.</returns>
         public float Stop()
         {
             ElapseTime();
@@ -186,26 +207,42 @@ namespace PrairieShellStudios.Timer
             return currentTime;
         }
 
+        /// <summary>
+        /// Get the stop time of the timer.
+        /// </summary>
+        /// <returns>A positive value representing the stop time for the timer.</returns>
+        public float StopTime()
+        {
+            return (direction == TimerDirection.CountUp) ? maxTime : minTime;
+        }
+
+        override public string ToString()
+        {
+            return "Start: " + StartTime() + "\tCurrent: " + currentTime + "\tStop: " + StopTime() + "\n";
+        }
+
         #endregion
 
         #region logic
 
         /// <summary>
-        /// Elapses the timer in the appropriate direction unless the time limit has been reached or the timer is not active.
+        /// Elapse the timer in the appropriate direction, unless the time limit has been reached or the timer is not active.
         /// </summary>
         private void ElapseTime()
         { 
             if (isActive)
             {
                 float tempTime = currentTime;
-
+                Debug.Log("Is active");
                 if (direction == TimerDirection.CountUp)
                 {
+                    Debug.Log("Adding time");
                     tempTime += Time.deltaTime;
                     currentTime = (tempTime < timeLimit) ? tempTime : timeLimit;
                 }
                 else
                 {
+                    Debug.Log("Decreasing time");
                     tempTime -= Time.deltaTime;
                     currentTime = (tempTime > timeLimit) ? tempTime : timeLimit;
                 }
@@ -218,7 +255,7 @@ namespace PrairieShellStudios.Timer
     #region enums
 
     /// <summary>
-    /// Classifies a timer as either counting up or counting down.
+    /// Classify a timer as either counting up or counting down.
     /// </summary>
     public enum TimerDirection { CountUp, CountDown }
 
--- Yeddie Summit/Assets/Tests/PlayMode/SimpleTimerTests.cs
+++ Yeddie Summit/Assets/Tests/PlayMode/SimpleTimerTests.cs
@@ -7,91 +7,276 @@ using PrairieShellStudios.Timer;
 
 public class SimpleTimerTests
 {
+    #region fields
 
-    private float stopWaitTime = 0.25f;
+    static private float defaultStartTime = 0f;
+    static private float stopWaitTime;
+    static private float testDuration = 0.25f;
+    static private float testStartTime = 1f;
+    static private float unlimitedStopTime = -1f;
 
-    [Test]
-    public void UnlimitedDefaultTimerTest()
+    static private List<TestTimerWrapper> listTimerWrappers = new List<TestTimerWrapper>();
+
+    static TestTimerWrapper[] GetTimerWrappers() 
     {
+        stopWaitTime = testDuration / 2f;
+
+        UnlimitedDefault();
+        UnlimitedWithStartTime();
+        CountUpWithLimit();
+        CountDownWithLimit();
+        CountUpWithLimitAndRange();
+        CountDownWithLimitAndRange();
+
+        return listTimerWrappers.ToArray(); 
+    }
+
+    #endregion
+
+    #region setup and teardowns
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        stopWaitTime = testDuration / 2f;
+    }
+
+    #endregion
+
+    #region static methods
+
+    static void UnlimitedDefault()
+    {
+        Debug.Log("running unlimited default");
+        listTimerWrappers.Add(new TestTimerWrapper(new SimpleTimer(), false, TimerDirection.CountUp, 
+            defaultStartTime, unlimitedStopTime));
+    }
+
+     static void UnlimitedWithStartTime()
+     {
+        Debug.Log("running unlimited with start time");
+        listTimerWrappers.Add(
+            new TestTimerWrapper(
+            new SimpleTimer(testStartTime), 
+            false, TimerDirection.CountUp, testStartTime, unlimitedStopTime));
+     }
+
+     static void CountUpWithLimit()
+     {
+        Debug.Log("running countup with limit");
+        listTimerWrappers.Add(
+            new TestTimerWrapper(
+            new SimpleTimer(defaultStartTime, defaultStartTime + testDuration), 
+            true, TimerDirection.CountUp, defaultStartTime, defaultStartTime + testDuration));
+     }
+
+     static void CountDownWithLimit()
+     {
+        Debug.Log("running countdown with limit");
+        listTimerWrappers.Add(
+            new TestTimerWrapper(
+            new SimpleTimer(TimerDirection.CountDown, testDuration),
+            true, TimerDirection.CountDown, testDuration, defaultStartTime));
+     }
+
+     static void CountUpWithLimitAndRange()
+     {
+        Debug.Log("running countup with limit and range");
+        listTimerWrappers.Add(
+            new TestTimerWrapper(
+                new SimpleTimer(TimerDirection.CountUp, testStartTime, testStartTime + testDuration),
+                true, TimerDirection.CountUp, testStartTime, testStartTime + testDuration));
+     }
+
+     static void CountDownWithLimitAndRange()
+     {
+        Debug.Log("running countdown with limit and range");
+        listTimerWrappers.Add(
+            new TestTimerWrapper(
+                new SimpleTimer(TimerDirection.CountDown, testStartTime + testDuration, testStartTime),
+                true, TimerDirection.CountDown, testStartTime + testDuration, testStartTime));
+     }
+
+    /*[Test]
+    public void CountUpWithDurationChangeTest()
+    {
+        // create an unlimited timer
         SimpleTimer timer = new SimpleTimer();
-        Assert.NotNull(timer);
-        TimerTest(timer, false, 0f, -1f, stopWaitTime);
+        TimerTests(timer, false, TimerDirection.CountUp, defaultStartTime, unlimitedStopTime);
+        // change the duration
+        timer.ChangeDuration(testDuration);
+        TimerTests(timer, true, TimerDirection.CountUp, defaultStartTime, testDuration);
     }
 
     [Test]
-    public void UnlimitedTimerWithStartTimeTest()
+    public void CountDownwithDurationChangeTest()
     {
-        SimpleTimer timer = new SimpleTimer();
-        Assert.NotNull(timer);
-        TimerTest(timer, false, 1f, -1f, stopWaitTime);
+        // create a countdown timer
+        SimpleTimer timer = new SimpleTimer(TimerDirection.CountDown, testDuration);
+        // increase the duration
+        testDuration *= 2f;
+        timer.ChangeDuration(testDuration);
+        TimerTests(timer, true, TimerDirection.CountDown, testDuration, defaultStartTime);
+        // decrease the duration
+        testDuration /= 2f;
+        timer.ChangeDuration(testDuration);
+        TimerTests(timer, true, TimerDirection.CountDown, testDuration, defaultStartTime);
+    }*/
+
+    #endregion
+
+    #region tests
+
+    [Test]
+    public void NegativeTimersTest()
+    {
+        // negative start time
+        SimpleTimer testTimer = new SimpleTimer(-testStartTime);
+        Assert.AreEqual(testStartTime, testTimer.MinTime);
+        // negative start and negative stop time
+        testTimer = new SimpleTimer(-testStartTime, -testStartTime - testDuration);
+        Assert.AreEqual(testStartTime, testTimer.MinTime);
+        Assert.AreEqual(testStartTime + testDuration, testTimer.MaxTime);
+        // count-down: negative start and negative stop time
+        testTimer = new SimpleTimer(TimerDirection.CountDown, -testStartTime, -testStartTime - testDuration);
+        Assert.AreEqual(testStartTime, testTimer.MinTime);
+        Assert.AreEqual(testStartTime + testDuration, testTimer.MaxTime);
     }
 
     [Test]
-    public void TimerWithLimitTest()
+    public void NegativeDurationChangeTest()
     {
-        SimpleTimer timer = new SimpleTimer(1f, 1.25f);
-        Assert.NotNull(timer);
-        TimerTest(timer, true, 1f, 1.25f, 0.125f);
+        // create a default timer
+        SimpleTimer testTimer = new SimpleTimer();
+        // change the duration to be a negative value
+        testTimer.ChangeDuration(-2 * testDuration);
+        Assert.AreEqual(2 * testDuration, testTimer.MaxTime);
+        // create a countdown timer
+        testTimer = new SimpleTimer(TimerDirection.CountDown, defaultStartTime, testDuration);
+        // change the duration to be a negative value
+        testTimer.ChangeDuration(-2 * testDuration);
+        Assert.AreEqual(2 * testDuration, testTimer.MaxTime);
     }
 
-
-    [Test]
-    public void CountUpTimerWithLimitTest()
+    [UnityTest, Sequential]
+    public IEnumerator TimerTests([ValueSourceAttribute(nameof(GetTimerWrappers))] TestTimerWrapper timerWrapper)
     {
-        SimpleTimer timer = new SimpleTimer(TimerDirection.CountUp, 0.25f);
+        Debug.Log("New Timer");
+        SimpleTimer timer = timerWrapper.Timer;
+        float startTime = timerWrapper.StartTime;
+        float stopTime = timerWrapper.StopTime;
+        bool hasLimit = timerWrapper.HasLimit;
+        TimerDirection direction = timerWrapper.Direction;
+
         Assert.NotNull(timer);
-        TimerTest(timer, true, 0f, 0.25f, 0.125f);
-    }
-
-    [Test]
-    public void CountDownTimerWithLimitTest()
-    {
-        SimpleTimer timer = new SimpleTimer(TimerDirection.CountDown, 0.25f);
-        Assert.NotNull(timer);
-        TimerTest(timer, true, 0.25f, 0f, 0.125f);
-    }
-
-    [Test]
-    public void CountUpTimerWithLimitAndRangeTest()
-    {
-        SimpleTimer timer = new SimpleTimer(TimerDirection.CountUp, 1f, 1.25f);
-        Assert.NotNull(timer);
-        TimerTest(timer, true, 1f, 1.25f, 0.125f);
-    }
-
-    [Test]
-    public void CountDownTimerWithLimitAndRangeTest()
-    {
-        SimpleTimer timer = new SimpleTimer(TimerDirection.CountDown, 1.25f, 1f);
-        Assert.NotNull(timer);
-        TimerTest(timer, true, 1.25f, 1f, 0.125f);
-    }
-
-    public IEnumerator TimerTest(SimpleTimer timer, bool hasLimit, float startTime, float stopTime, float waitTime)
-    {
+        Debug.Log("Start Time = " + timer.ToString());
         // start timer
-        Assert.AreEqual(timer.CurrentTime, startTime);
-        Assert.AreEqual(timer.Start(), startTime);
+        Assert.AreEqual(startTime, timer.CurrentTime);
+        // verify durations
+        if (!hasLimit)
+        {
+            Assert.True(timer.Duration() < 0);
+        }
+        else
+        {
+            Assert.AreEqual(testDuration, timer.Duration());
+        }
+        // verify start and stop times with min and max times
+        if (direction == TimerDirection.CountUp)
+        {
+            if (hasLimit)
+            {
+                Assert.AreEqual(stopTime, timer.MaxTime);
+            }
+            Assert.AreEqual(startTime, timer.MinTime);
+        }
+        else
+        {
+            Assert.AreEqual(stopTime, timer.MinTime);
+            Assert.AreEqual(startTime, timer.MaxTime);
+        }
+
+        Assert.AreEqual(startTime, timer.Start());
         // wait for half timer duration
-        yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(stopWaitTime);
         Assert.IsFalse(timer.HasExpired());
         // stop timer
+        Debug.Log("First stop = " + timer.ToString());
         float recordedStopTime = timer.Stop();
+        /*Debug.Log(timer.ToString());*/
+
         yield return new WaitForSeconds(stopWaitTime);
         Assert.AreEqual(recordedStopTime, timer.CurrentTime);
         // start timer again and go until expired
         if (hasLimit)
         {
+            timer.Start();
             // wait for remainder of duration
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(stopWaitTime);
             Assert.IsTrue(timer.HasExpired());
-            Assert.AreEqual(timer.CurrentTime, stopTime);
+            Assert.AreEqual(stopTime, timer.CurrentTime);
             // stop timer one last time
             timer.Stop();
+            Debug.Log("Last Stop = " + timer.ToString());
         }
         // reset timer
         timer.Reset();
-        Assert.AreEqual(timer.CurrentTime, 0f);
+        Assert.AreEqual(startTime, timer.CurrentTime);
     }
 
+    #endregion
+
 }
+
+#region class(es)
+
+public class TestTimerWrapper
+{
+    #region fields
+
+    private SimpleTimer timer;
+    private bool hasLimit;
+    private TimerDirection direction;
+    private float startTime;
+    private float stopTime;
+
+    #endregion
+
+    #region constructor(s)
+
+    public TestTimerWrapper(SimpleTimer testTimer)
+    {
+        timer = testTimer;
+        direction = testTimer.Direction;
+        hasLimit = (Direction == TimerDirection.CountUp) ? testTimer.MaxTime != -1 : true;
+        startTime = testTimer.StartTime();
+        stopTime = testTimer.StopTime();
+    }
+
+    public TestTimerWrapper(SimpleTimer testTimer, bool hasLimit, TimerDirection direction, float startTime, float stopTime)
+    {
+        timer = testTimer;
+        this.direction = direction;
+        this.HasLimit = hasLimit;
+        this.startTime = startTime;
+        this.stopTime = stopTime;
+    }
+
+    #endregion
+    
+    #region properties
+
+    public SimpleTimer Timer { get => timer; set => timer = value; }
+
+    public bool HasLimit { get => hasLimit; set => hasLimit = value; }
+        
+    public TimerDirection Direction { get => direction; set => direction = value; }
+
+    public float StartTime { get => startTime; set => startTime = value; }
+
+    public float StopTime { get => stopTime; set => stopTime = value; }
+
+    #endregion
+}
+
+#endregion
